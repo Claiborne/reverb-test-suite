aaa8e4ebb6ef3efff21d79a9b80594e259f277b0
Will Claiborne <willclaiborne@gmail.com>
2013-09-16 18:16:20 -0700

    one-off script for getting article IDs

---
+++ scripts/scrape_article_ids.rb
@@ -0,0 +1,14 @@
+require 'rest_client'
+require 'json'
+
+%w(0).each do |skip|
+
+	response = RestClient.get "https://dev-api.helloreverb.com/v2/trending/tiles/me?format=json&skip=#{skip}&limit=150&api_key=e891a91f35d84fbe4cce308ef9844c8eb35c956a15f17cd5"
+
+	data = JSON.parse(response)
+
+	data['tiles'].each do |article|
+		puts article['contentId']
+	end
+
+end
