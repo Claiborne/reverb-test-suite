ac71abda0f65c563b040adcc6dec323203ded59d
Will Claiborne <willclaiborne@gmail.com>
2013-09-20 21:01:35 -0700

    fix formatting issues caused by ide

--- functional/spec/bifrost/_user_flows/get_interest_streams.rb
+++ functional/spec/bifrost/_user_flows/get_interest_streams.rb
@@ -9,78 +9,78 @@ include Token
 
 describe "Get Interests Streams" do
 
-	class Interests
-		@me = []; @global = []
-		class << self; attr_accessor :me, :global; end
-	end
+  class Interests
+    @me = []; @global = []
+    class << self; attr_accessor :me, :global; end
+  end
 
-	before(:all) do
-		# Get bifrost environment
-		ConfigPath.config_path = File.dirname(__FILE__) + "/../../../config/bifrost.yml"
-		@bifrost_env = "https://#{ConfigPath.new.options['baseurl']}"
+  before(:all) do
+    # Get bifrost environment
+    ConfigPath.config_path = File.dirname(__FILE__) + "/../../../config/bifrost.yml"
+    @bifrost_env = "https://#{ConfigPath.new.options['baseurl']}"
 
-		# Set headers
-		@headers = {:content_type => 'application/json', :accept => 'application/json'}
+    # Set headers
+    @headers = {:content_type => 'application/json', :accept => 'application/json'}
 
-		# Get anon session token
-		@session_token = get_anon_token(@bifrost_env)
-	end
+    # Get anon session token
+    @session_token = get_anon_token(@bifrost_env)
+  end
 
   it 'should get 24 me interest values' do
- 	  url = @bifrost_env+"/trending/interests/me?skip=0&limit=100&api_key="+@session_token
- 	  begin
- 	  	response = RestClient.get url, @headers
- 	  rescue => e
- 	  	raise StandardError.new(e.message+":\n"+url)
- 	  end
- 	  interests = (JSON.parse response)['interests']
- 	  interests.each {|i| Interests.me << i['value']}
- 	  Interests.me.length.should == 24
+    url = @bifrost_env+"/trending/interests/me?skip=0&limit=100&api_key="+@session_token
+    begin
+      response = RestClient.get url, @headers
+    rescue => e
+      raise StandardError.new(e.message+":\n"+url)
+    end
+    interests = (JSON.parse response)['interests']
+    interests.each {|i| Interests.me << i['value']}
+    Interests.me.length.should == 24
   end
 
   it 'should get 24 global interest values' do
- 	  url = @bifrost_env+"/trending/interests/global?skip=0&limit=100&api_key="+@session_token
- 	  begin
- 	  	response = RestClient.get url, @headers
- 	  rescue => e
- 	  	raise StandardError.new(e.message+":\n"+url)
- 	  end
- 	  interests = (JSON.parse response)['interests']
- 	  interests.each {|i| Interests.global << i['value']}
- 	  Interests.global.length.should == 24
+    url = @bifrost_env+"/trending/interests/global?skip=0&limit=100&api_key="+@session_token
+    begin
+      response = RestClient.get url, @headers
+    rescue => e
+      raise StandardError.new(e.message+":\n"+url)
+    end
+    interests = (JSON.parse response)['interests']
+    interests.each {|i| Interests.global << i['value']}
+    Interests.global.length.should == 24
   end
 
   it "should return data for each 'me' interest" do
-  	errors = []
+    errors = []
     Interests.me.each do |interest|
-    	url = @bifrost_env+"/interests/stream?interest=#{CGI::escape interest}&skip=0&limit=50&api_key="+@session_token
- 	  	begin
- 	  		response = RestClient.get url, @headers
- 	  	rescue RestClient::ResourceNotFound => e
- 	  		errors << url
- 	  		next
- 	  	rescue => e
- 	  		raise StandardError.new(e.message+":\n"+url)
- 	  	end
-    	data = JSON.parse response
-			data['tiles'].length.should > 0
+      url = @bifrost_env+"/interests/stream?interest=#{CGI::escape interest}&skip=0&limit=50&api_key="+@session_token
+      begin
+        response = RestClient.get url, @headers
+      rescue RestClient::ResourceNotFound => e
+        errors << url
+        next
+      rescue => e
+        raise StandardError.new(e.message+":\n"+url)
+      end
+      data = JSON.parse response
+      data['tiles'].length.should > 0
     end
     errors.should == []
   end
 
   it "should return data for each 'global' interest" do
-  	errors = []
+    errors = []
     Interests.global.each do |interest|
-    	url = @bifrost_env+"/interests/stream?interest=#{CGI::escape interest}&skip=0&limit=50&api_key="+@session_token
- 	  	begin
- 	  		response = RestClient.get url, @headers
- 	  	rescue RestClient::ResourceNotFound => e
- 	  		errors << url
- 	  	rescue => e
- 	  		raise StandardError.new(e.message+":\n"+url)
- 	  	end
-    	data = JSON.parse response
-    	data['tiles'].length.should > 0
+      url = @bifrost_env+"/interests/stream?interest=#{CGI::escape interest}&skip=0&limit=50&api_key="+@session_token
+      begin
+        response = RestClient.get url, @headers
+      rescue RestClient::ResourceNotFound => e
+        errors << url
+      rescue => e
+        raise StandardError.new(e.message+":\n"+url)
+      end
+      data = JSON.parse response
+      data['tiles'].length.should > 0
     end
     errors.should == []
   end
--- scripts/scrape_article_ids.rb
+++ scripts/scrape_article_ids.rb
@@ -3,12 +3,12 @@ require 'json'
 
 %w(0).each do |skip|
 
-	response = RestClient.get "https://dev-api.helloreverb.com/v2/trending/tiles/me?format=json&skip=#{skip}&limit=150&api_key=e891a91f35d84fbe4cce308ef9844c8eb35c956a15f17cd5"
+  response = RestClient.get "https://dev-api.helloreverb.com/v2/trending/tiles/me?format=json&skip=#{skip}&limit=150&api_key=e891a91f35d84fbe4cce308ef9844c8eb35c956a15f17cd5"
 
-	data = JSON.parse(response)
+  data = JSON.parse(response)
 
-	data['tiles'].each do |article|
-		puts article['contentId']
-	end
+  data['tiles'].each do |article|
+    puts article['contentId']
+  end
 
 end
