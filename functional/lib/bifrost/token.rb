0038d7d5c770134d394afae53daf285f76c74980
Will Claiborne <willclaiborne@gmail.com>
2013-09-20 21:01:35 -0700

    fix formatting issues caused by ide

--- functional/lib/bifrost/token.rb
+++ functional/lib/bifrost/token.rb
@@ -3,16 +3,15 @@ require 'json'
 
 module Token
 
-	def get_anon_token(base_url)
+  def get_anon_token(base_url)
     endpoint = "#{base_url}/account/ohai?format=json"
     body = {"deviceId"=>"reverb-test-suite"}.to_json
     begin 
-    	response = RestClient.post endpoint, body, :content_type => "application/json"
+      response = RestClient.post endpoint, body, :content_type => "application/json"
     rescue => e
-    	raise StandardError.new(e.message+" "+endpoint)
+      raise StandardError.new(e.message+" "+endpoint)
     end
     data = JSON.parse response
     data['token']
-	end
-
+  end
 end
--- functional/spec/bifrost/trending/smoke.rb
+++ functional/spec/bifrost/trending/smoke.rb
@@ -7,34 +7,34 @@ require 'bifrost/token.rb'
 describe "Trending API -- Smoke", :smoke => true do
 
   before(:all) do
-  	# Get bifrost environment
+    # Get bifrost environment
     ConfigPath.config_path = File.dirname(__FILE__) + "/../../../config/bifrost.yml"
     @bifrost_env = "https://#{ConfigPath.new.options['baseurl']}"
 
     # Set headers
-		@headers = {:content_type => 'application/json', :accept => 'application/json'}
-    
+    @headers = {:content_type => 'application/json', :accept => 'application/json'}
+
     # Get anon session token
     @session_token = get_anon_token(@bifrost_env)	
-	end
+  end
 
-	it 'should get me interests for an anon session' do
+  it 'should get me interests for an anon session' do
     interest_type = @bifrost_env+"/trending/interests/me?skip=0&limit=100&api_key="+@session_token
-		begin
-			response = RestClient.get interest_type, @headers
+    begin
+      response = RestClient.get interest_type, @headers
     rescue => e
-    	raise StandardError.new(e.message+" "+interest_type)
+      raise StandardError.new(e.message+" "+interest_type)
     end
     data = JSON.parse response
     data['interests'].length.should > 0
-	end
+  end
 
-	it 'should get global interests for an anon session' do
+  it 'should get global interests for an anon session' do
     interest_type = @bifrost_env+"/trending/interests/global?skip=0&limit=100&api_key="+@session_token
-		begin
-			response = RestClient.get interest_type, @headers
+    begin
+      response = RestClient.get interest_type, @headers
     rescue => e
-    	raise StandardError.new(e.message+" "+interest_type)
+      raise StandardError.new(e.message+" "+interest_type)
     end
     data = JSON.parse response
     data['interests'].length.should > 0
