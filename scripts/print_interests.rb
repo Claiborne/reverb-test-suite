0f4f29eba16600d3716e52506194e814322edf61
Will Claiborne <willclaiborne@gmail.com>
2013-10-21 14:28:56 -0700

    renamed things production to stage

--- scripts/print_interests.rb
+++ scripts/print_interests.rb
@@ -2,7 +2,7 @@ require 'rest-client'
 require 'json'
 
 
-res = RestClient.get "https://api.helloreverb.com/v2/trending/interests/global?limit=300&api_key=ce4adfb352ddac2d7f6724f224824dcd8ff6a64d77400133&format=json"
+res = RestClient.get "https://stage-api.helloreverb.com/v2/trending/interests/global?limit=300&api_key=ce4adfb352ddac2d7f6724f224824dcd8ff6a64d77400133&format=json"
 data = JSON.parse res
 data['interests'].each do |interest|
   puts interest['value']
