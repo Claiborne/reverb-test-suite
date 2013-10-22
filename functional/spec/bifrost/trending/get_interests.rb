991e53ced4ac63e16f1f98158cb33544e5e8cecc
Will Claiborne <willclaiborne@gmail.com>
2013-10-22 14:22:56 -0700

    adding more bifrost tests for trending

--- functional/spec/bifrost/trending/get_interests.rb
+++ functional/spec/bifrost/trending/get_interests.rb
@@ -87,3 +87,62 @@ describe "TRENDING API -- Get 'Global' Interests For Anon User" do
     interest_values.should == interest_values.uniq
   end
 end
+
+describe "TRENDING API -- Get 'Me' Interests for Logged in User" do
+
+  before(:all) do
+
+    # Get bifrost environment
+    ConfigPath.config_path = File.dirname(__FILE__) + "/../../../config/bifrost.yml"
+    @bifrost_env = "https://#{ConfigPath.new.options['baseurl']}"
+
+    # Set headers
+    @headers = {:content_type => 'application/json', :accept => 'application/json'}
+
+    # Get anon session token
+    @session_token = get_anon_token(@bifrost_env) 
+
+    # Get logged in session token
+    @session_token_logged_in = get_token @bifrost_env, 'clay00', 'testpassword'
+
+    # Get Interests for Logged-in User
+    url = @bifrost_env+"/trending/interests/me?skip=0&limit=25&api_key="+@session_token_logged_in
+    begin
+      response = RestClient.get url, @headers
+    rescue => e
+      raise StandardError.new(e.message+" "+url)
+    end
+    @data_logged_in = JSON.parse response
+
+    # Get Interests for Anon User
+    url = @bifrost_env+"/trending/interests/me?skip=0&limit=25&api_key="+@session_token
+    begin
+      response = RestClient.get url, @headers
+    rescue => e
+      raise StandardError.new(e.message+" "+url)
+    end
+    @data_anon = JSON.parse response
+  end
+
+  it "should get 25 'me' interests" do
+    @data_logged_in['interests'].length.should == 25  
+  end
+
+  it "should get different interests from an anon user" do
+    logged_in_interests = []
+    anon_interests = []
+
+    # Get logged-in interests
+    @data_logged_in['interests'].each do |interest|
+      logged_in_interests << interest['value']
+    end
+
+    # Get anon interests
+    @data_anon['interests'].each do |interest|
+      anon_interests << interest['value']
+    end
+
+    # Compare logged-in interests to anon interests
+    logged_in_interests.should_not == anon_interests
+  end
+end
