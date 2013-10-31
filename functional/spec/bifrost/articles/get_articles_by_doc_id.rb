1a31f4ed352c5b0150bdbddf85c4834536335cff
Will Claiborne <willclaiborne@gmail.com>
2013-10-30 17:32:15 -0700

    remove test tag from last commit

--- functional/spec/bifrost/articles/get_articles_by_doc_id.rb
+++ functional/spec/bifrost/articles/get_articles_by_doc_id.rb
@@ -4,7 +4,7 @@ require 'rest_client'
 require 'json'
 require 'api_checker.rb'; include APIChecker
 
-describe "ARTICLES API -- GET Articles by docId", :test => true do
+describe "ARTICLES API -- GET Articles by docId" do
 
   before(:all) do
     # Get bifrost environment
