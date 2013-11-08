c4b65879f6980aab4a1d09d979dbdc4d2edd8948
Will Claiborne <willclaiborne@gmail.com>
2013-11-08 12:50:28 -0800

    update create users script

--- scripts/create_users.rb
+++ scripts/create_users.rb
@@ -1,20 +1,26 @@
 require 'rest-client'
 require 'json'
 
-env = "https://dev-api.helloreverb.com/v2"
-endpoint = "/account/register?clientId=51561484e4b0edfcfec11627"
 
-(0..2500).each do |n|
+env = "https://stage-api.helloreverb.com/v2"
+endpoint = "/account/register?clientId=515b32b0e4b03f3544d60a15&format=json"
+name = 'clay1test'
+
+(23001..24000).each do |n|
   body = {
-    "login"=>"clayt3#{n}",
+    "login"=>"#{name}#{n}",
     "password"=>"testpassword",
     "passwordConfirmation"=>"testpassword",
-    "email"=>"claytest3#{n}@faketestuser.com",
-    "name"=>"clayt3#{n}",
+    "email"=>"#{name}#{n}@faketestuser.com",
+    "name"=>"#{name}#{n}",
     "deviceId"=>"reverb-test-suite"
   }.to_json
-
-  RestClient.post env+endpoint, body, :content_type => 'application/json'
-  puts n
-
+  begin
+    r = RestClient.post env+endpoint, body, :content_type => 'application/json', :accpet => :json
+  rescue
+    next
+  end
+  d = JSON.parse r
+  puts d['userId']
 end
+  
