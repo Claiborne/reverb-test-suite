f5f486eaf051abe424e0a983417b6eb52f97fdc1
Will Claiborne <willclaiborne@gmail.com>
2013-11-12 17:00:09 -0800

    update clean content checker to flag bad word articles by date

--- scripts/check_clean_content.rb
+++ scripts/check_clean_content.rb
@@ -7,6 +7,10 @@ require '../functional/lib/bifrost/token.rb'; include Token
 
 # Search corpus for bad words in title
 
+raise StandardError, "\nNOTE: Command line argument is required: YYYY-MM-DD".yellow unless ARGV[0].to_s.match(/\d\d\d\d-\d\d-\d\d/)
+
+date = ARGV[0].to_s
+
 bad_words = []
 flagged_content = []
 
@@ -15,18 +19,20 @@ File.open(File.dirname(__FILE__)+'/bad_words.txt', "r").each_line do |line|
 end
 
 bad_words.each do |bad_word|
-  puts bad_word.yellow
-  flagged_content << "--------------- #{bad_word} ---------------"
-  url = URI::encode "https://insights.helloreverb.com/proxy/corpus-service/api/corpus.json/searchDocs?skip=0&limit=100&searchType=prefix&searchField=title&searchString=#{bad_word}&excludeReviewedDocs=false"
-  res = RestClient.get url, :content_type => 'application/json', :Authorization => 'Basic d2NsYWlib3JuZTpyZXZlcmJ0ZXN0MTIz'
-  data = JSON.parse res
-  puts "::::: #{bad_word} :::::".red if data.count > 99
-  data.each do |d|
-    flagged_content << d['title']
+  %w(0 100 200 300 400 500).each do |skip|
+    puts "."
+    flagged_content << "--------------- #{bad_word} ---------------"
+    url = URI::encode "https://insights.helloreverb.com/proxy/corpus-service/api/corpus.json/searchDocs?skip=#{skip}&limit=100&searchType=prefix&searchField=title&searchString=#{bad_word}&excludeReviewedDocs=false"
+    res = RestClient.get url, :content_type => 'application/json', :Authorization => 'Basic d2NsYWlib3JuZTpyZXZlcmJ0ZXN0MTIz'
+    data = JSON.parse res
+    data.each do |d|
+      (flagged_content << d['title']) if d['createDate'].match(date)
+    end
+    break unless data.count > 99
   end
 end
 
-File.open('/Users/willclaiborne/Desktop/flagged_article_titles', 'w') do |f|  
+File.open("/Users/willclaiborne/Desktop/flagged_article_titles_#{date}", 'w') do |f|  
   f.puts flagged_content
 end  
 
