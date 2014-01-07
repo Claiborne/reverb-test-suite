ae9a78a85d7ec2240845395cff8de7275908143c
Will Claiborne <willclaiborne@gmail.com>
2014-01-07 13:23:18 -0800

    mail Aaron and me if web failiures

--- scripts/mail_test_results_helloreverb.rb
+++ scripts/mail_test_results_helloreverb.rb
@@ -8,7 +8,7 @@ file.each {|line|
 
 FROM_EMAIL = "reverbqualityassurance@gmail.com"
 PASSWORD = "testpassword"
-TO_EMAIL = ["wclaiborne@helloreverb.com","willclaiborne@gmail.com"]
+TO_EMAIL = ["aaron@wordnik.com","wclaiborne@helloreverb.com"]
 
 msgstr = <<END_OF_MESSAGE
 From: Reverb QA <#{FROM_EMAIL}>
