7ae04d94138612d09c1acf08982eaa12cb94fa25
Will Claiborne <willclaiborne@gmail.com>
2014-01-14 15:18:45 -0800

    email willf if bifrost trending duplicates fail

--- scripts/mail_test_results_trending_tile_dups.rb
+++ scripts/mail_test_results_trending_tile_dups.rb
@@ -8,7 +8,7 @@ file.each {|line|
 
 FROM_EMAIL = "reverbqualityassurance@gmail.com"
 PASSWORD = "testpassword"
-TO_EMAIL = ["wclaiborne@helloreverb.com","wclaiborne@helloreverb.com"]
+TO_EMAIL = "will@helloreverb.com"
 
 msgstr = <<END_OF_MESSAGE
 From: Reverb QA <#{FROM_EMAIL}>
