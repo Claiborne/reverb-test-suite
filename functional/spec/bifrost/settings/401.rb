8453b518ebada1808286a260ea557eb220c864cd
Will Claiborne <willclaiborne@gmail.com>
2014-02-19 16:08:53 -0800

    adding bifrost homescreen specs

--- functional/spec/bifrost/settings/401.rb
+++ functional/spec/bifrost/settings/401.rb
@@ -1,1 +1,22 @@
+require 'rspec'
+require 'config_path'
+require 'rest_client'
+require 'json'
 
+describe "SETTINGS API - GET /settings/homescreen with bad auth key" do
+
+  before(:all) do
+    ConfigPath.config_path = File.dirname(__FILE__) + "/../../../config/bifrost.yml"
+    @config = ConfigPath.new
+    @url_no_key = "https://#{@config.options['baseurl']}/settings/homescreen"
+    @url_invalid_key = "https://#{@config.options['baseurl']}/settings/homescreen?api_key=123"
+  end
+
+  it "should 401 when no auth key" do
+    expect {RestClient.get @url_no_key}.to raise_error(RestClient::Unauthorized)
+  end
+  
+  it "should 401 when invalid auth key" do
+    expect {RestClient.get @url_invalid_key}.to raise_error(RestClient::Unauthorized)
+  end
+end
