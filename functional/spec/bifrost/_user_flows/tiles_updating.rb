07a82c6868d27d29beddddb80557480420633b59
Will Claiborne <wclaiborne@helloreverb.com>
2014-03-19 15:09:38 -0700

    in bifrost specs, check first page of me tiles has an article no more than a day old, instread of checking at least one of the first 5 articles are within 2 hours

--- functional/spec/bifrost/_user_flows/tiles_updating.rb
+++ functional/spec/bifrost/_user_flows/tiles_updating.rb
@@ -66,20 +66,19 @@ describe "USER FLOWS - Check Trending Tiles Are Updating", :tiles_updating => tr
     time_difference.should < 60*60*2
   end
 
-  it 'should return a trending me article no more than 2 hours old for signed-in user' do
+  it 'should return a trending me article, in the first page, no more than 1 day old for signed-in user' do
     article_pub_dates = []
     @signed_in_me_tiles['tiles'].each do |t|
       article_pub_dates << Time.parse(t['publishDate']).to_i if t['publishDate']
-      break if article_pub_dates.length == 5
     end
-    article_pub_dates.length.should > 4
+    article_pub_dates.length.should > 10
 
     time_differences = []
 
     article_pub_dates.each do |date|
       time_differences << Time.now.utc.to_i - date
     end
-    puts time_differences.sort.first.should < 60*60*2
+    puts time_differences.sort.first.should < 60*60*24
   end
   
   it 'should return first trending social article no more than 2 hours old' do
