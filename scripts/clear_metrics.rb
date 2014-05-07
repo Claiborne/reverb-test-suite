1a70b49c1c536638a66a60d36add5b7b7edf7750
Will Claiborne <wclaiborne@helloreverb.com>
2014-05-07 13:57:59 -0700

    scripts for monitoring audit metrics and clearing them

---
+++ scripts/clear_metrics.rb
@@ -0,0 +1,20 @@
+require 'rest-client'
+require 'json'
+require 'colorize'
+require 'pp'
+
+case ARGV[0]
+when 'prd'
+  url = "https://api.helloreverb.com/v2/clearMetrics"
+when 'stg'
+  url = "https://stage-api.helloreverb.com/v2/clearMetrics"
+when 'dev'
+  url = "https://dev-api.helloreverb.com/v2/clearMetrics"
+else
+  raise StandardError, "Need an ARGV[0] for which Bifrost env metrics to reset: prd, stg, or dev"
+end
+
+6.times do
+  RestClient.delete url
+  sleep 0.2
+end
