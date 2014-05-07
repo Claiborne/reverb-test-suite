d805ce0198db456f651abbf7f8f652fb37cbdd72
Will Claiborne <wclaiborne@helloreverb.com>
2014-05-07 14:30:51 -0700

    add interest search to audit metrics monitor

--- scripts/monitor_audit.rb
+++ scripts/monitor_audit.rb
@@ -16,7 +16,7 @@ end
 raise StandardError, "Need an ARGV[1] for how many seconds you want to sleep before querying audit metrics again" unless ARGV[1]
 
 puts "News Wordwall\t|News Tiles\t|Me WordWall\t|Me Tiles\t|Article\t|Article Recs\t|Homescreen\t|Chirp\t|Profile\t|\
-Collection\t|User Collections\t|Collection Recs\t|Interest Stream".yellow
+Collection\t|User Collections\t|Collection Recs\t|Interest Stream\t|Interest Search".yellow
 
 1000.times do  
   r = RestClient.get url, :content_type => 'application/json'
@@ -35,9 +35,11 @@ Collection\t|User Collections\t|Collection Recs\t|Interest Stream".yellow
   user_collections = begin; data['com.wordnik.bifrost.apis.CollectionsApi']['/collection/get-collections']['duration']['median'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
   collection_recs = begin; data['com.wordnik.bifrost.apis.CollectionsApi']['/collection/recommendations']['duration']['median'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
   interest_stream = begin; data['com.wordnik.bifrost.apis.InterestsApi']['/interests/stream']['duration']['median'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
+  interest_search = begin; data['com.wordnik.bifrost.apis.InterestsApi']['/interests/search']['duration']['median'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
 
   puts "#{news_wordwall}\t\t|#{news_tiles}\t\t|#{me_wordwall}\t\t|#{me_tiles}\t\t|#{article}\t\t|#{article_recs}\t\t|".green+
-       "#{homescreen}\t\t|#{chirp}\t|#{profile}\t\t|#{collection}\t\t|#{user_collections}\t\t\t|#{collection_recs}\t\t\t|#{interest_stream}".green
+       "#{homescreen}\t\t|#{chirp}\t|#{profile}\t\t|#{collection}\t\t|#{user_collections}\t\t\t|#{collection_recs}\t\t\t".green+
+       "|#{interest_stream}\t\t\t|#{interest_search}".green
 
   sleep ARGV[1].to_i
 end
