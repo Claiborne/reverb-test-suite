d3a8da2923d741148424a91815968cf81687d9b3
Will Claiborne <wclaiborne@helloreverb.com>
2014-05-08 14:05:28 -0700

    modify test script for checking new tiles pub date

--- scripts/check_tiles_sort.rb
+++ scripts/check_tiles_sort.rb
@@ -5,11 +5,13 @@ require 'json'
 @limit = 24
 @api_key = 'a13931ff647dbb20239c8c385fa0421688e88ccce172903c'
 
-40.times do
+200.times do
   #url = "https://stage-api.helloreverb.com/v2/trending/tiles/me?skip=#{@skip}&limit=#{@limit}&api_key=#{@api_key}&format=json"
   url = "https://api.helloreverb.com/v2/trending/tiles/global?skip=#{@skip}&api_key=#@api_key&format=json"
   res = RestClient.get url
   data = JSON.parse res
+  puts @skip
+  break if data['tiles'].count == 0
   @skip = @skip + data['tiles'].count
   data['tiles'].each do |d|
     puts d['publishDate'].match(/\d\d\d\d-\d\d-\d\d/).to_s if d['tileType'] == 'article'
