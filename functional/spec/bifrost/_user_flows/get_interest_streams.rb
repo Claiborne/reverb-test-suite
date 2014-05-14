4fad950d359eb8e6d5b03f0fce308f4f132a278a
Will Claiborne <wclaiborne@helloreverb.com>
2014-05-14 10:37:23 -0700

    news word wall should return 135 instead of 215

--- functional/spec/bifrost/_user_flows/get_interest_streams.rb
+++ functional/spec/bifrost/_user_flows/get_interest_streams.rb
@@ -38,7 +38,7 @@ describe "USER FLOWS - Get Trending interests For an Anon User" do
     Interests_Helper.me.length.should == 24
   end
 
-  it 'should return at least 215 global interests (FAILS IN PRODUCTION RVB-4231)' do
+  it 'should return at least 135 global interests' do
     url = @bifrost_env+"/trending/interests/global?limit=500&api_key="+@session_token
     begin
       response = RestClient.get url, @headers
@@ -47,8 +47,8 @@ describe "USER FLOWS - Get Trending interests For an Anon User" do
     end
     interests = (JSON.parse response)['interests']
     interests.each {|i| Interests_Helper.global << i['value']}
-    interests.count.should > 214
-    Interests_Helper.global.length.should > 214
+    interests.count.should > 134
+    Interests_Helper.global.length.should > 134
   end
 
   it "should return 24 articles for each 'me' topic" do
