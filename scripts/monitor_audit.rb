a106a1f100de61b2669fe53bb419074e1bf6faba
Will Claiborne <wclaiborne@helloreverb.com>
2014-05-20 09:48:56 -0700

    Bifrost audit metrics monitor now monitors login and ohai, and now out puts average instead of median

--- scripts/monitor_audit.rb
+++ scripts/monitor_audit.rb
@@ -16,30 +16,32 @@ end
 raise StandardError, "Need an ARGV[1] for how many seconds you want to sleep before querying audit metrics again" unless ARGV[1]
 
 puts "News Wordwall\t|News Tiles\t|Me WordWall\t|Me Tiles\t|Article\t|Article Recs\t|Homescreen\t|Chirp\t|Profile\t|\
-Collection\t|User Collections\t|Collection Recs\t|Interest Stream\t|Interest Search".yellow
+Collection\t|User Collections\t|Collection Recs\t|Interest Stream\t|Interest Search\t|Login\t|ohai".yellow
 
 1000.times do  
   r = RestClient.get url, :content_type => 'application/json'
   data = JSON.parse r
 
-  news_wordwall = begin; data['com.wordnik.bifrost.apis.TrendingApi']['/trending/interests/global']['duration']['median'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
-  news_tiles = begin; data['com.wordnik.bifrost.apis.TrendingApi']['/trending/tiles/global']['duration']['median'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
-  me_wordwall = begin; data['com.wordnik.bifrost.apis.TrendingApi']['/trending/interests/me']['duration']['median'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
-  me_tiles = begin; data['com.wordnik.bifrost.apis.TrendingApi']['/trending/tiles/me']['duration']['median'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
-  article = begin; data['com.wordnik.bifrost.apis.ArticlesApi']['/articles/docId']['duration']['median'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
-  article_recs = begin; data['com.wordnik.bifrost.apis.ArticlesApi']['/articles/recommendations']['duration']['median'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
-  homescreen = begin; data['com.wordnik.bifrost.apis.SettingsApi']['/homescreen']['duration']['median'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
-  chirp = begin; data['com.wordnik.bifrost.apis.AccountApi']['/account/chirpWithAuthSession']['duration']['median'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
-  profile = begin; data['com.wordnik.bifrost.apis.UserProfileApi']['myProfile']['duration']['median'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
-  collection = begin; data['com.wordnik.bifrost.apis.CollectionsApi']['/collection/get-collection']['duration']['median'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
-  user_collections = begin; data['com.wordnik.bifrost.apis.CollectionsApi']['/collection/get-collections']['duration']['median'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
-  collection_recs = begin; data['com.wordnik.bifrost.apis.CollectionsApi']['/collection/recommendations']['duration']['median'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
-  interest_stream = begin; data['com.wordnik.bifrost.apis.InterestsApi']['/interests/stream']['duration']['median'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
-  interest_search = begin; data['com.wordnik.bifrost.apis.InterestsApi']['/interests/search']['duration']['median'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
+  news_wordwall = begin; data['com.wordnik.bifrost.apis.TrendingApi']['/trending/interests/global']['duration']['mean'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
+  news_tiles = begin; data['com.wordnik.bifrost.apis.TrendingApi']['/trending/tiles/global']['duration']['mean'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
+  me_wordwall = begin; data['com.wordnik.bifrost.apis.TrendingApi']['/trending/interests/me']['duration']['mean'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
+  me_tiles = begin; data['com.wordnik.bifrost.apis.TrendingApi']['/trending/tiles/me']['duration']['mean'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
+  article = begin; data['com.wordnik.bifrost.apis.ArticlesApi']['/articles/docId']['duration']['mean'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
+  article_recs = begin; data['com.wordnik.bifrost.apis.ArticlesApi']['/articles/recommendations']['duration']['mean'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
+  homescreen = begin; data['com.wordnik.bifrost.apis.SettingsApi']['/homescreen']['duration']['mean'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
+  chirp = begin; data['com.wordnik.bifrost.apis.AccountApi']['/account/chirpWithAuthSession']['duration']['mean'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
+  profile = begin; data['com.wordnik.bifrost.apis.UserProfileApi']['myProfile']['duration']['mean'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
+  collection = begin; data['com.wordnik.bifrost.apis.CollectionsApi']['/collection/get-collection']['duration']['mean'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
+  user_collections = begin; data['com.wordnik.bifrost.apis.CollectionsApi']['/collection/get-collections']['duration']['mean'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
+  collection_recs = begin; data['com.wordnik.bifrost.apis.CollectionsApi']['/collection/recommendations']['duration']['mean'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
+  interest_stream = begin; data['com.wordnik.bifrost.apis.InterestsApi']['/interests/stream']['duration']['mean'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
+  interest_search = begin; data['com.wordnik.bifrost.apis.InterestsApi']['/interests/search']['duration']['mean'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
+  login = begin; data['com.wordnik.bifrost.apis.AccountApi']['/account/oauthLogin']['duration']['mean'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
+  ohai = begin; data['com.wordnik.bifrost.apis.AccountApi']['/account/ohai']['duration']['mean'].to_s.match(/^\d{0,}/).to_s; rescue; 0; end
 
   puts "#{news_wordwall}\t\t|#{news_tiles}\t\t|#{me_wordwall}\t\t|#{me_tiles}\t\t|#{article}\t\t|#{article_recs}\t\t|".green+
        "#{homescreen}\t\t|#{chirp}\t|#{profile}\t\t|#{collection}\t\t|#{user_collections}\t\t\t|#{collection_recs}\t\t\t".green+
-       "|#{interest_stream}\t\t\t|#{interest_search}".green
+       "|#{interest_stream}\t\t\t|#{interest_search}\t\t\t|#{login}\t|#{ohai}".green
 
   sleep ARGV[1].to_i
 end
