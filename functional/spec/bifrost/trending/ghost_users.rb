c3e0cfd08da6eaf390a46d17c895d5d8053eb542
Will Claiborne <wclaiborne@helloreverb.com>
2014-05-22 11:53:39 -0700

    bifrost specs to check for duplicate ghost users in social tiles and word wall

--- functional/spec/bifrost/trending/ghost_users.rb
+++ functional/spec/bifrost/trending/ghost_users.rb
@@ -35,7 +35,7 @@ describe "TRENDING - Ghost Users" do
     @social_tiles = JSON.parse response
   end
 
-  it "should display in social interests" do
+  it "should return in social interests" do
     ghost_users = []
     @social_interests['interests'].each do |social_interest|
       ghost_users << social_interest if social_interest['interestType'] == 'person'
@@ -43,7 +43,7 @@ describe "TRENDING - Ghost Users" do
     ghost_users.count.should > 0
   end
 
-  it "should display at least 4 ghost users in all social interests" do
+  it "should return at least 4 ghost users in all social interests" do
     ghost_users = []
     @social_interests['interests'].each do |social_interest|
       ghost_users << social_interest if social_interest['interestType'] == 'person'
@@ -51,19 +51,51 @@ describe "TRENDING - Ghost Users" do
     ghost_users.count.should > 3
   end
 
-  it "should display in social tiles" do
+  it "should not return duplicate ghost users in social interests" do
+    ghost_users = []
+    @social_interests['interests'].each do |social_interest|
+      ghost_users << social_interest['displayName'] if social_interest['interestType'] == 'person'
+    end
+    ghost_users.count.should > 0
+    ghost_users.should == ghost_users.uniq
+  end
+
+  it "should return in social tiles" do
     ghost_users = []
     @social_tiles['tiles'].each do |social_tiles|
       ghost_users << social_tiles if social_tiles['tileType'] == 'person'
     end
     ghost_users.count.should > 0
   end
-  it "should display at least 2 ghost users in first 24 social tiles" do
+
+  it "should return at least 2 ghost users in first 24 social tiles" do
     ghost_users = []
     @social_tiles['tiles'].each do |social_tiles|
       ghost_users << social_tiles if social_tiles['tileType'] == 'person'
     end
-    ghost_users.count.should > 1
+    ghost_users.count.should > 0
+  end
+
+  it "should not return duplicate ghost users in first 500 social tiles" do
+    ghost_users = []
+    skip = 0
+
+    21.times do 
+      url = @bifrost_env+"/trending/tiles/social?skip=#{skip}&api_key="+@session
+      begin
+        response = RestClient.get url, @headers
+      rescue => e
+        raise StandardError.new(e.message+" "+url)
+      end
+      social_tiles = JSON.parse response
+      break if social_tiles['tiles'].count < 1
+      skip = skip + social_tiles['tiles'].count
+      social_tiles['tiles'].each do |social_tile|
+        ghost_users << social_tile['header']['value'] if social_tile['tileType'] == 'person'
+      end
+    end
+    ghost_users.count.should > 0
+    ghost_users.should == ghost_users.uniq
   end
 
 end
