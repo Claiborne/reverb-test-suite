960ba92a295ba98665461927ac2deeba1e398ec4
Will Claiborne <wclaiborne@helloreverb.com>
2014-07-15 13:03:16 -0700

    more odin speca

---
+++ functional/spec/odin/image_only.rb
@@ -0,0 +1,45 @@
+require 'bunny'
+require 'json'
+require 'pp'
+require 'rspec'
+require 'securerandom'
+require 'odin/odin_shared_examples.rb'
+require 'odin/odin_spec_helper.rb'; include OdinSpecHelper
+
+describe "Article ingestion - image-only doc" do
+
+  before(:all) do
+
+    @timeout = 15
+    @notification_count_break = 7
+
+    @request_id = SecureRandom.uuid.to_s
+    @url_submitted = 'http://odin-integration.helloreverb.com/smoke_articles/image_only.html'
+    @odin_notifications = []
+
+    @conn = Bunny.new(:host => "localhost", :port => 5672)
+
+    @conn.start
+    @ch = @conn.create_channel
+    q = @ch.queue('', :exclusive => true)
+    q.bind('online-messaging', :routing_key => 'global.urlIngestionResult')
+      q.subscribe(:ack => true) do |delivery_info, properties, payload|
+      odin_notification = JSON.parse payload
+      @odin_notifications << odin_notification if odin_notification['requestId'] == @request_id
+    end
+
+    @message = {
+      "eventName" => 'com.reverb.events.odin.package$Submission',
+      "requestId" => @request_id,
+      "url" => @url_submitted,
+      "source" => "ReverbTestSuite"
+    }.to_json
+  end
+
+  after(:all) {@conn.close}
+
+  include_examples 'Shared all'
+
+  include_examples 'Shared filtered'
+
+end
