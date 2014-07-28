0a730ed4ca6ae2a57cef5ca68ff5056320b9876d
Will Claiborne <wclaiborne@helloreverb.com>
2014-07-28 16:21:58 -0700

    mailer for stage results

---
+++ scripts/mail_test_results_stg.rb
@@ -0,0 +1,28 @@
+require 'net/smtp'
+
+name = ARGV[0]
+
+file = File.open("/home/wclaiborne/#{name}-stg-results.txt")
+contents = ""
+contents << Time.now.to_s
+contents << ""
+file.each {|line|
+  contents << line
+}
+
+FROM_EMAIL = "reverbqualityassurance@gmail.com"
+PASSWORD = "testpassword"
+TO_EMAIL = ["qa@helloreverb.com","marco@helloreverb.com"]
+
+msgstr = <<END_OF_MESSAGE
+From: Reverb QA <#{FROM_EMAIL}>
+To: QA <#{TO_EMAIL}>
+Subject: #{name.capitalize} Results in Production
+#{contents}
+END_OF_MESSAGE
+
+smtp = Net::SMTP.new 'smtp.gmail.com', 587
+smtp.enable_starttls
+smtp.start('gmail.com', 'reverbqualityassurance', PASSWORD, :login) do |smtp|
+  smtp.send_message msgstr, FROM_EMAIL, TO_EMAIL
+end
