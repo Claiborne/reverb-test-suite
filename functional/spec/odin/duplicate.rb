c4bd57ea5895afebb7466ff3c18de169dd61d7a1
Will Claiborne <wclaiborne@helloreverb.com>
2014-07-28 16:01:44 -0700

    update odin specs

--- functional/spec/odin/duplicate.rb
+++ functional/spec/odin/duplicate.rb
@@ -6,7 +6,7 @@ require 'securerandom'
 require 'odin/odin_shared_examples.rb'
 require 'odin/odin_spec_helper.rb'; include OdinSpecHelper
 
-describe "Article ingestion - duplicate doc" do
+describe "Article ingestion - duplicate doc", :duplicate => true do
 
   class OdinDocHelper
     class << self; attr_accessor :doc; end
@@ -77,6 +77,114 @@ describe "Article ingestion - duplicate doc" do
 
       @request_id = SecureRandom.uuid.to_s
       @url_submitted = 'http://odin-integration.helloreverb.com/smoke_articles/duplicate.html'
+      @odin_notifications = []
+
+      @conn = Bunny.new(:host => "localhost", :port => 5672)
+
+      @conn.start
+      @ch = @conn.create_channel
+      q = @ch.queue('', :exclusive => true)
+      q.bind('online-messaging', :routing_key => 'global.urlIngestionResult')
+        q.subscribe(:ack => true) do |delivery_info, properties, payload|
+        odin_notification = JSON.parse payload
+        @odin_notifications << odin_notification if odin_notification['requestId'] == @request_id
+      end
+
+      @message = {
+        "eventName" => 'com.reverb.events.odin.package$Submission',
+        "requestId" => @request_id,
+        "url" => @url_submitted,
+        "source" => "ReverbTestSuite"
+      }.to_json
+    end
+
+    after(:all) {@conn.close}
+
+    include_examples 'Submit'
+
+    include_examples 'Shared correlated and parsed' 
+
+    it 'should return the same correlated.expandedUri value as submitted' do
+      correlated = extractNotification @odin_notifications, 'correlated'
+      correlated['correlated']['expandedUri'].should == @url_submitted
+    end
+
+    include_examples 'Shared filtered with docFilterOkay'
+
+    include_examples 'Shared all'
+
+    it 'should filter the document as a duplicate' do
+      filtered = extractNotification @odin_notifications, 'filtered'
+      filtered['filtered']['reasons'][0]['rule']['name'].should == 'Deduplication'
+    end
+
+    include_examples 'Debug'
+
+  end # end context
+
+  context 'submit duplicate-at-beginning document' do
+
+    before(:all) do
+
+      $counter = 0
+      @timeout = 60
+
+      @request_id = SecureRandom.uuid.to_s
+      @url_submitted = 'http://odin-integration.helloreverb.com/smoke_articles/duplicate_at_beginning.html'
+      @odin_notifications = []
+
+      @conn = Bunny.new(:host => "localhost", :port => 5672)
+
+      @conn.start
+      @ch = @conn.create_channel
+      q = @ch.queue('', :exclusive => true)
+      q.bind('online-messaging', :routing_key => 'global.urlIngestionResult')
+        q.subscribe(:ack => true) do |delivery_info, properties, payload|
+        odin_notification = JSON.parse payload
+        @odin_notifications << odin_notification if odin_notification['requestId'] == @request_id
+      end
+
+      @message = {
+        "eventName" => 'com.reverb.events.odin.package$Submission',
+        "requestId" => @request_id,
+        "url" => @url_submitted,
+        "source" => "ReverbTestSuite"
+      }.to_json
+    end
+
+    after(:all) {@conn.close}
+
+    include_examples 'Submit'
+
+    include_examples 'Shared correlated and parsed' 
+
+    it 'should return the same correlated.expandedUri value as submitted' do
+      correlated = extractNotification @odin_notifications, 'correlated'
+      correlated['correlated']['expandedUri'].should == @url_submitted
+    end
+
+    include_examples 'Shared filtered with docFilterOkay'
+
+    include_examples 'Shared all'
+
+    it 'should filter the document as a duplicate' do
+      filtered = extractNotification @odin_notifications, 'filtered'
+      filtered['filtered']['reasons'][0]['rule']['name'].should == 'Deduplication'
+    end
+
+    include_examples 'Debug'
+
+  end # end context
+
+  context 'submit duplicate-but-shorter document' do
+
+    before(:all) do
+
+      $counter = 0
+      @timeout = 60
+
+      @request_id = SecureRandom.uuid.to_s
+      @url_submitted = 'http://odin-integration.helloreverb.com/smoke_articles/duplicate_but_shorter.html'
       @odin_notifications = []
 
       @conn = Bunny.new(:host => "localhost", :port => 5672)
