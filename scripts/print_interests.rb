a890dca36e0335b4b94c08f1fecab4afee7ccc64
Will Claiborne <wclaiborne@helloreverb.com>
2014-07-29 20:20:27 -0700

    smarter wordwall printer

--- scripts/print_interests.rb
+++ scripts/print_interests.rb
@@ -1,9 +1,19 @@
 require 'rest-client'
 require 'json'
+require 'colorize'
 
+if ARGV[0] == 'prd'
+  key = '72b8bd6a95a93fe871934d93e1b442e4ba95396b151a05f2' 
+  domain = 'api.helloreverb.com'
+end
 
-res = RestClient.get "https://stage-api.helloreverb.com/v2/trending/interests/global?limit=300&api_key=ce4adfb352ddac2d7f6724f224824dcd8ff6a64d77400133&format=json"
+if ARGV[0] == 'stg'
+  key = 'ce4adfb352ddac2d7f6724f224824dcd8ff6a64d77400133'
+  domain = 'stage-api.helloreverb.com'
+end
+
+res = RestClient.get "https://#{domain}/v2/trending/interests/global?limit=15&api_key=#{key}&format=json"
 data = JSON.parse res
 data['interests'].each do |interest|
-  puts interest['value']
+  puts "#{interest['value']}".green
 end
