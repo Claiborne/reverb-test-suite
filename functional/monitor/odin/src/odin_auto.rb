98f3ea2a5d2fffd4da1877d7a0d6e54d8e320c3b
Will Claiborne <wclaiborne@helloreverb.com>
2014-09-17 18:47:52 -0700

    add odin mailing list back to Odin SWF Basic Monitoring

--- functional/monitor/odin/src/odin_auto.rb
+++ functional/monitor/odin/src/odin_auto.rb
@@ -36,7 +36,7 @@ contents << get_failure_breakdown_improved
 
 FROM_EMAIL = "reverbqualityassurance@gmail.com"
 PASSWORD = "testpassword"
-TO_EMAIL = ["wclaiborne@helloreverb.com"]
+TO_EMAIL = ["wclaiborne@helloreverb.com", "odin@helloreverb.com"]
 
 msgstr = <<END_OF_MESSAGE
 From: Reverb QA (Do not reply) <#{FROM_EMAIL}>
